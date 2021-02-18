dfae0069cd1c5f48df7805c7868506e45500cda8
RichterElric <elric.richter@gmail.com>
2021-02-18 11:44:04 +0100

    server with db connexion

--- server/index.js
+++ server/index.js
@@ -3,4 +3,25 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const express = require('express')
+const app = express();
+
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+
+const CONNEXION_URL = "mongodb://127.0.0.1:27017/Projet_Web"
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(CONNEXION_URL, { useNewUrlParser: true,useUnifiedTopology:true })
+    .then(()=> {
+        app.listen(PORT,()=> {
+            console.log(`server running on port : ${PORT}`)
+        })
+    })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+mongoose.set('useFindAndModify',false)
+
+const db = mongoose.connection
